d2aa36ef603f10d983b4b6851883e1b99e03a25e
AntoineD <dangleterre.antoine@gmail.com>
2024-04-21 20:38:16 +0200

    Add dark mode for the component

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,5 +1,5 @@
 interface ExperienceProps {
-  post: string, 
+  post: string,
   dateStart: number,
   dateEnd?: number,
   company: string,
@@ -8,12 +8,14 @@ interface ExperienceProps {
 
 function Experience({ post, company, dateStart, dateEnd, children }: ExperienceProps) {
   return (
-    <div className="bg-lightSecondary-300 rounded-xl p-6 sm:p-8 md:p-10">
+    <div className="bg-lightSecondary-300 dark:bg-[#163E6C] rounded-xl p-6 sm:p-8 md:p-10">
       <div className="flex flex-col md:flex-row md:gap-6 gap-1">
-        <div className="flex text-lightPrimary-700 flex-none">{dateStart} - {dateEnd ? dateEnd : "Auj."}</div>
+        <div className="flex text-lightPrimary-700 dark:text-darkPrimary-700 flex-none">{dateStart} - {dateEnd ? dateEnd : "Auj."}</div>
         <div className="flex flex-col gap-3">
-          <h3 className="text-lightPrimary-900 font-openSans flex-0 font-bold text-lg mb-2">{post} | {company}</h3>
-          {children}
+          <h3 className="text-lightPrimary-900 dark:text-darkPrimary-900 font-openSans flex-0 font-bold text-lg mb-2">{post} | {company}</h3>
+          <div className="dark:text-[#BCD2EF]">
+            {children}
+          </div>
         </div>
       </div>
     </div>
