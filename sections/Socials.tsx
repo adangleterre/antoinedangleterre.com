0e9c8f42b6e5b62c40072d8a646019b07327a283
AntoineD <dangleterre.antoine@gmail.com>
2024-04-22 10:33:11 +0200

    Add sizing control for icons component

--- sections/Socials.tsx
+++ sections/Socials.tsx
@@ -4,13 +4,13 @@ function Socials() {
   return (
     <div className="flex gap-12 mb-[50px] items-center">
       <a href="https://github.com/adangleterre" target="_blank">
-        <Icons name={"github"}/>
+        <Icons name={"github"} size={30}/>
       </a>
       <a href="http://fr.linkedin.com/in/antoine-dangleterre" target="_blank">
-        <Icons name={"linkedin"}/>
+        <Icons name={"linkedin"} size={30}/>
       </a>
       <a href="mailto:bonjour@antoinedangleterre.com" target="_blank">
-        <Icons name={"mail"}/>
+        <Icons name={"mail"} size={25}/>
       </a>
     </div>
   )
