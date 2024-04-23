519459567daea9f119dba98afc3e672a36cd1c92
AntoineD <dangleterre.antoine@gmail.com>
2024-04-23 15:46:35 +0200

    Fix spacing

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -16,17 +16,8 @@ interface ProjectCardProps {
 function ProjectCard({ name, description, directLink, githubLink, image, imagePosition, technologies = [] }: ProjectCardProps) {
     if (imagePosition === "left") {
         return (
-            <div className="flex relative">
-                <div className="relative w-7/12 hidden md:block">
-                    <Image
-                        alt={`capture d'écran du projet intitulé ${name}`}
-                        height={800}
-                        width={800}
-                        src={projectImage}
-                        className="rounded-[10px] brightness-75"
-                    />
-                </div>
-                <div className="bg-[#EFF6FF] rounded-[10px] p-11 flex flex-col gap-6 w-12/12 md:w-7/12 shadow-projectCard z-10 md:absolute right-0 top-1/2 transform -translate-y-1/2">
+            <div className="flex items-center relative flex-row-reverse xl:min-h-[500px]">
+                <div className="bg-[#EFF6FF] rounded-[10px] p-11 flex flex-col gap-6 w-12/12 md:w-7/12 shadow-projectCard z-10 h-min">
                     <p className="font-bold text-xl">{name}</p>
                     <p className="text-[#4B5563]">{description}</p>
                     <div className="flex flex-wrap gap-x-2">
@@ -48,13 +39,31 @@ function ProjectCard({ name, description, directLink, githubLink, image, imagePo
                             }
                         </div>
                     }
+                </div>
+                <div className="w-7/12 hidden md:block md:absolute left-0 top-1/2 transform -translate-y-1/2">
+                    <Image
+                        alt={`capture d'écran du projet intitulé ${name}`}
+                        height={800}
+                        width={800}
+                        src={projectImage}
+                        className="rounded-[10px] brightness-75"
+                    />
                 </div>
             </div>
         )
     } else if (imagePosition === "right") {
         return (
-            <div className="flex relative flex-row-reverse">
-                <div className="bg-[#EFF6FF] rounded-[10px] p-11 flex flex-col gap-6 w-12/12 md:w-7/12 shadow-projectCard z-10 md:absolute top-1/2 left-0 transform -translate-y-1/2">
+            <div className="flex items-center relative xl:min-h-[500px]">
+                <div className="w-7/12 hidden md:block md:absolute right-0 top-1/2 transform -translate-y-1/2">
+                    <Image
+                        alt={`capture d'écran du projet intitulé ${name}`}
+                        height={800}
+                        width={800}
+                        src={projectImage}
+                        className="rounded-[10px] brightness-75"
+                    />
+                </div>
+                <div className="bg-[#EFF6FF] rounded-[10px] p-11 flex flex-col gap-6 w-12/12 md:w-7/12 shadow-projectCard z-10 h-min">
                     <p className="font-bold text-xl">{name}</p>
                     <p className="text-[#4B5563]">{description}</p>
                     <div className="flex flex-wrap gap-x-2">
@@ -77,19 +86,10 @@ function ProjectCard({ name, description, directLink, githubLink, image, imagePo
                         </div>
                     }
                 </div>
-                <div className="relative w-7/12 hidden md:block">
-                    <Image
-                        alt={`capture d'écran du projet intitulé ${name}`}
-                        height={800}
-                        width={800}
-                        src={projectImage}
-                        className="rounded-[10px] brightness-75"
-                    />
-                </div>
             </div>
         )
     }
-    
+
 }
 
 export default ProjectCard
