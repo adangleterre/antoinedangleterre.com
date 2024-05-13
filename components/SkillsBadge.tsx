dc6840c04ff1c47af79c8200e8d5e36023f84896
AntoineD <dangleterre.antoine@gmail.com>
2024-05-13 12:18:13 +0200

    Fix skills data responsability and randomized them

--- components/SkillsBadge.tsx
+++ components/SkillsBadge.tsx
@@ -1,16 +1,15 @@
-type SkillsProps = {
-    skills: string[];
-}
+function SkillsBadge() {
+    const skills = ["ReactJs", "NextJs", "JavaScript", "TypeScript","TailwindCSS", "Bootstrap", "Git", "Github", "Figma", "Adobe XD", "Accessibilité numérique"]
+    const shuffledSkills = skills.sort(() => Math.random() - 0.5);
 
-function SkillsBadge({ skills = [] }: SkillsProps) {
     return (
-        <>
-            {skills.map((skill, index) => (
-                <div key={index}>
+        <div className="flex">
+            {shuffledSkills.map((skill, index) => (
+                <div key={index} className="mx-2 rounded-md text-lg font-semibold bg-[#B4D0F0] text-primary-900 dark:text-[#000000] dark:bg-darkSecondary-300 px-4 py-1 cursor-default">
                     {skill}
                 </div>
             ))}
-        </>
+        </div>
     )
 }
 
