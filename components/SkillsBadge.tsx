7a09eaeeea0058eeb1ae8a990b995dc18622525e
AntoineD <dangleterre.antoine@gmail.com>
2024-05-14 16:21:25 +0200

    Add sequential framer animations to skills marquee

--- components/SkillsBadge.tsx
+++ components/SkillsBadge.tsx
@@ -1,13 +1,40 @@
+import { motion } from "framer-motion";
+
 function SkillsBadge() {
-    const skills = ["ReactJs", "NextJs", "JavaScript", "TypeScript","TailwindCSS", "Bootstrap", "Git", "Github", "Figma", "Adobe XD", "Accessibilité numérique"]
+    const skills = ["ReactJs", "NextJs", "JavaScript", "TypeScript", "TailwindCSS", "Bootstrap", "Git", "Github", "Figma", "Adobe XD", "Accessibilité numérique"]
     const shuffledSkills = skills.sort(() => Math.random() - 0.5);
 
+    const animationVariants = {
+        initial: {
+            opacity: 0,
+            y: 20,
+        },
+        animate: (index: number) => ({
+            opacity: 1,
+            y: 0,
+            transition: {
+                delay: 1 + (0.10 * index),
+            }
+        }),
+    };
+
     return (
-        <div className="flex">
+        <div className="flex overflow-hidden">
             {shuffledSkills.map((skill, index) => (
-                <div key={index} className="mx-2 rounded-md text-lg font-semibold bg-[#B4D0F0] text-primary-900 dark:text-[#000000] dark:bg-darkSecondary-300 px-4 py-1 cursor-default">
+                <motion.div
+                    key={index}
+                    variants={animationVariants}
+                    initial="initial"
+                    animate="animate"
+                    whileInView="animate"
+                    viewport={{
+                        once: true,
+                    }}
+                    custom={index}
+                    className="mx-2 overflow-hidden rounded-md shadow-lightButton dark:shadow-darkButton text-lg font-semibold bg-[#B4D0F0] text-primary-900 dark:text-[#000000] dark:bg-darkSecondary-300 px-4 py-1 my-3 cursor-default"
+                >
                     {skill}
-                </div>
+                </motion.div>
             ))}
         </div>
     )
