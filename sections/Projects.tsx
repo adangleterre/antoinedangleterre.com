80d74283f76f9f74c8cca84b5851be72d7d4d6aa
AntoineD <dangleterre.antoine@gmail.com>
2024-05-31 11:43:10 +0200

    Fix typos and add front-end project

--- sections/Projects.tsx
+++ sections/Projects.tsx
@@ -1,21 +1,21 @@
 import ProjectCard from "@/components/ProjectCard"
-import Reaveal from "@/components/Reaveal"
+import RevealContent from "@/components/ReavealContent"
 import SectionBadge from "@/components/SectionBadge"
 
 function Projects() {
   return (
     <>
-      <Reaveal>
+      <RevealContent>
         <>
           <SectionBadge sectionName={"Mes projets"} />
           <h2 className="text-3xl font-extrabold text-center mb-6 md:mb-10">Quelques projets personnels</h2>
         </>
-      </Reaveal>
+      </RevealContent>
       <div className="flex flex-col gap-12 lg:gap-20">
         <ProjectCard
           name={"antoinedangleterre.com"}
-          description={"Explorez mon site personnel dédié au développement, un projet en constante évolution où je partage mes créations, mes expérimentations et mon parcours dans le monde de la programmation."}
-          technologies={["Next JS", "React", "Tailwind CSS", "API", "Responsive", "Shadcn"]}
+          description={"Mon site personnel dédié au développement, un projet en constante évolution où je partage mes projets personnels, mon parcours, ainsi que mes informations de contact."}
+          technologies={["Next JS", "React.JS", "Tailwind CSS", "Framer motion", "Responsive", "Shadcn"]}
           imageSrc={"/images/Screenshot-Antoine-Dangleterre.png"}
           imagePosition={"right"}
           githubLink={"https://github.com/adangleterre/antoinedangleterre.com"}
@@ -24,18 +24,26 @@ function Projects() {
         <ProjectCard
           name={"La data verte"}
           description={"Un site web créé en collaboration avec un autre developpeur, ce site récupère les données d'une API custom pour afficher des statistiques et des informations sur le club de foot de l'AS Saint-Etienne."}
-          technologies={["NextJS", "React", "Tailwind CSS", "API", "Responsive", "Github Actions"]}
+          technologies={["NextJS", "React.JS", "Tailwind CSS", "API", "Responsive", "Github Actions"]}
           imageSrc={"/images/Screenshot-La-data-verte.png"}
           imagePosition={"left"}
           githubLink={"https://github.com/adangleterre/la-data-verte"}
           directLink={"https://la-data-verte.vercel.app/"}
         />
         <ProjectCard
+          name={"Front-end practice"}
+          description={"Frontendpractice.com est un site proposant des challenges pour les développeurs front-end. L'objectif est simple : recréer des sites existants d'entreprises réelles telles que Trello, Starbucks, Shopify, Nintendo, etc."}
+          technologies={["React.JS", "Tailwind CSS", "Responsive"]}
+          imageSrc={"/images/Screenshot-Frontend-practice.png"}
+          imagePosition={"right"}
+          githubLink={"https://github.com/adangleterre/FrontEndPractice"}
+        />
+        <ProjectCard
           name={"Kodiz"}
           description={"Le site mon auto-entreprise avec laquelle j'ai créé et designé des sites pour des entreprises de tous horizons"}
           technologies={["Blog", "SEO", "Marketing", "Webdesign", "UX/UI", "WordPress", "Maintenance"]}
           imageSrc={"/images/Screenshot-kodiz.png"}
-          imagePosition={"right"}
+          imagePosition={"left"}
           directLink={"https://kodiz.fr"}
         />
       </div>
