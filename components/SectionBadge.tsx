9ff65c638fa8ef2ea169c0f49294d664af01fc38
AntoineD <dangleterre.antoine@gmail.com>
2024-05-31 11:25:37 +0200

    Fix cursor style on hover for badges

--- components/SectionBadge.tsx
+++ components/SectionBadge.tsx
@@ -5,7 +5,7 @@ interface sectionBadgeProps {
 function SectionBadge({ sectionName }: sectionBadgeProps) {
   return (
     <div className="flex justify-center mb-3">
-      <div className="rounded-[10px] bg-lightSecondary-300 text-primary-900 dark:text-[#E7F1FD] dark:bg-darkSecondary-300 px-3 py-1">
+      <div className="rounded-[10px] bg-lightSecondary-300 text-primary-900 dark:text-[#E7F1FD] dark:bg-darkSecondary-300 px-3 py-1 cursor-default">
         {sectionName}
       </div>
     </div>
