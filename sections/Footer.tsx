f6c82ff26c97001d6de074e69c44df95b4e7bf61
AntoineD <dangleterre.antoine@gmail.com>
2025-05-29 17:31:45 +0200

    Fix global cv link

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -41,7 +41,7 @@ function Footer() {
           bonjour@antoinedangleterre.fr
         </a>
         <a
-          href="https://drive.google.com/file/d/1tjdjfRHZ6Q07skRe19hsihYCxvw4dIuU/view?usp=sharing"
+          href="https://drive.google.com/file/d/1gauwXynx1Rp-BmBx5rjcZekhdoMB_Nq7/view?usp=sharing"
           target="_blank"
         >
           Consultez mon cv
