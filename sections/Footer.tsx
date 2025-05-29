9cd4a105a0e085a5f3964b456e1d76bdc7dd01c7
AntoineD <dangleterre.antoine@gmail.com>
2025-05-29 13:28:23 +0200

    Fix availability item to unavailable

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -40,7 +40,10 @@ function Footer() {
         <a href="mailto:bonjour@antoinedangleterre.com">
           bonjour@antoinedangleterre.fr
         </a>
-        <a href="https://drive.google.com/file/d/1tjdjfRHZ6Q07skRe19hsihYCxvw4dIuU/view?usp=sharing">
+        <a
+          href="https://drive.google.com/file/d/1tjdjfRHZ6Q07skRe19hsihYCxvw4dIuU/view?usp=sharing"
+          target="_blank"
+        >
           Consultez mon cv
         </a>
       </div>
